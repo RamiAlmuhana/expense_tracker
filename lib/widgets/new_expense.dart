722a8973b0bc5471159ea3651bef4e16ce7c3f31
Rami Al-Muhana <ramealmuhana@hotmail.com>
2025-03-02 21:08:19 +0100

    Alert toegevoegd zodat de gebruiker een error krijgt als hij niks invult

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -37,7 +37,23 @@ class _NewExpenseState extends State<NewExpense> {
     .text); // tryparse('Hello') => null, tryparse('1.12') => 1.12
     final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
     if (_titleController.text.trim().isEmpty || amountIsInvalid || _selectedDate == null) {
-      // show error meassage
+      print('aasas');
+      showDialog(
+        context: context, 
+        builder: (ctx) => AlertDialog(
+          title: const Text('Invalid input'),
+          content: const Text('Please make sure a valid title, amount, date and category was entered.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(ctx);
+              }, 
+              child: const Text('Okay'),
+            ),
+          ],
+        ),
+      );
+      return;
     }
   }
 
@@ -51,7 +67,7 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(16),
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
@@ -73,7 +89,7 @@ class _NewExpenseState extends State<NewExpense> {
                   ),
                 ),
               ),
-              SizedBox(width: 16,),
+              const SizedBox(width: 16,),
               Expanded(
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.end,
@@ -82,7 +98,7 @@ class _NewExpenseState extends State<NewExpense> {
                     Text(_selectedDate == null ? 'No date selected' : formatter.format(_selectedDate!)),
                     IconButton(
                       onPressed: _presentDatePicker, 
-                      icon: Icon(
+                      icon: const Icon(
                         Icons.calendar_month,
                       ),
                     ),
@@ -123,9 +139,7 @@ class _NewExpenseState extends State<NewExpense> {
                 child: const Text('Cancel')
               ),
               ElevatedButton(
-                onPressed: () {
-                  _submitExpenseData;
-                }, 
+                onPressed: _submitExpenseData,
                 child: const Text('Save Expense'),
               )
             ],
